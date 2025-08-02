a91d328665f70f1266be3b951c2755d09d545442
Damian <dkowalski.1997@gmail.com>
2025-08-03 00:43:45 +0200

    added insertion sort

--- insertion-sort/index.cjs
+++ insertion-sort/index.cjs
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+const insertionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const key = arr[i];
+    
+    let j = i - 1;
+
+    while (j >= 0 && key < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+const sorted = insertionSort([0, -10, 3, 2, 51, 21, 34]);
+console.log(JSON.stringify(sorted));
